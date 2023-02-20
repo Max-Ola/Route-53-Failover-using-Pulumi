3d954d851a1f8132fe8820e803efb6b6cec47352
Max-Ola <106966289+Max-Ola@users.noreply.github.com>
2023-02-20 13:55:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,98 @@
+import pulumi
+from pulumi_aws import ec2, rds, elb, autoscaling, route53
+
+# Define the regions to create the resources in
+regions = ["us-east-1", "us-east-2"]
+
+# Define the AMI to use for the EC2 instances
+ami = "ami-0c55b159cbfafe1f0"
+
+# Define the size of the EC2 instances
+instance_type = "t2.micro"
+
+# Define the size of the RDS instance
+db_instance_class = "db.t2.micro"
+
+# Define the database name and user
+db_name = "mydb"
+db_username = "admin"
+db_password = pulumi.secret("mysecret")
+
+# Define the Route 53 DNS name and domain
+dns_name = "mydomain.com"
+dns_domain = "mydomain.com"
+
+# Define the number of EC2 instances to create
+instance_count = 2
+
+# Define the security group to use for the EC2 instances and RDS instance
+sg = ec2.SecurityGroup("web-sg",
+    ingress=[ec2.SecurityGroupIngressArgs(
+        protocol="tcp",
+        from_port=80,
+        to_port=80,
+        cidr_blocks=["0.0.0.0/0"],
+    )]
+)
+
+# Define the launch configuration for the EC2 instances
+launch_config = autoscaling.LaunchConfiguration("web-lc",
+    image_id=ami,
+    instance_type=instance_type,
+    security_groups=[sg.id],
+    user_data="""#!/bin/bash
+                echo "Hello, World!" > index.html
+                nohup python -m SimpleHTTPServer 80 &
+                """
+)
+
+# Define the target group for the load balancer
+target_group = elb.TargetGroup("web-tg",
+    port=80,
+    protocol="HTTP",
+    target_type="instance",
+    vpc_id=ec2.get_vpc().id,
+)
+
+# Define the autoscaling group for the EC2 instances
+autoscaling_group = autoscaling.Group("web-asg",
+    launch_configuration=launch_config.id,
+    target_group_arns=[target_group.arn],
+    health_check_type="EC2",
+    health_check_grace_period=300,
+    min_size=instance_count,
+    max_size=instance_count,
+)
+
+# Define the RDS database instance
+database = rds.Instance("web-db",
+    engine="mysql",
+    engine_version="5.7",
+    instance_class=db_instance_class,
+    name=db_name,
+    username=db_username,
+    password=db_password,
+    skip_final_snapshot=True,
+    vpc_security_group_ids=[sg.id],
+)
+
+# Define the Route 53 record sets for the DNS failover policy
+record_sets = []
+for i, region in enumerate(regions):
+    zone = route53.get_zone(name=dns_domain, private_zone=False, region=region)
+    record_set = route53.RecordSet(f"web-rs-{i}",
+        name=dns_name,
+        type="A",
+        alias=[
+            route53.RecordSetAliasArgs(
+                name=elb.get_load_balancer(name="web-elb", region=region).dns_name,
+                zone_id=elb.get_load_balancer(name="web-elb", region=region).zone_id,
+            )
+        ],
+        zone_id=zone.zone_id,
+    )
+    record_sets.append(record_set)
+
+# Define the load balancer for the web application
+for region in regions:
+    lb = elb.LoadBalancer(f"web-elb-{region}",
